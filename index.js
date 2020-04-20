ab79e31e747628c731c3bef37944ddeba2b1843f
KianBadie <kianbadie@gmail.com>
2020-04-20 14:22:11 -0700

    Generate all urls for all issues associated with hack for la

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const request = require('request-promise');
+const querystring = require('querystring');
 const env = require('dotenv').config()
 
 var github = {
@@ -21,11 +22,12 @@ var github = {
           id: project.id,
           name: project.name,
           languages: { url: project.languages_url, data: [] },
-          contributors: { url: project.contributors_url, data: [] }
+          contributors: { url: project.contributors_url, data: [] },
+          issues: { url: project.issues_url.substring(0, project.issues_url.length-9), data: [] }
         });
       });
     }).catch(function(err) {
-        return err.message;
+      return err.message;
     });
   },
   getLanguageInfo: function(url) {
@@ -69,6 +71,54 @@ var github = {
     }).catch(function(err) {
         return err.message;
     });
+  },
+  getIssues: function(url){
+    return request({
+      "method": "GET",
+      "uri": url + "?state=all",
+      "json": true,
+      "resolveWithFullResponse": true,
+      "headers": {
+        "Authorization": "token " + github.token,
+        "User-Agent": github.userAgent
+      }
+    }).then(async function(response){
+      // Get the total amount of pages in the response
+      let issues = [];
+      let links = response.headers.link.split(",");
+      let lastPageString = links[1]; // NEED TO CHECK WHAT HAPPENS IF THERE IS ONLY ONE PAGE
+      let lastPageTrim = lastPageString.split(";")[0].trim();
+      let lastPageUrl = lastPageTrim.substring(1, lastPageTrim.length - 1);
+      let parsed = querystring.parse(lastPageUrl, "&", "=");
+      let lastPage = parsed.page;
+      // Loop through every page possible in response
+      for(i = 1; i <= lastPage; i++){
+        // Construct url
+        let url = `${response.request.href}&page=${i}`;
+        console.log(url);
+        issues.push(github.getIssuesHelper(url));
+      }
+      Promise.all(issues).then(function(result){
+        return Promise.resolve(result);
+      });
+    }).catch(function(err){
+      return err.message;
+    });
+  },
+  getIssuesHelper: function(url){
+    return request({
+      "method": "GET",
+      "uri": url,
+      "json": true,
+      "headers": {
+        "Authorization": "token " + github.token,
+        "User-Agent": github.userAgent
+      }
+    }).then(function(body){
+      return Promise.resolve(body);
+    }).catch(function(err){
+      return err.message;
+    });
   }
 }
 
@@ -80,10 +130,13 @@ async function main(params) {
   await github.getAllTaggedRepos();
   let lps = [], ldone = false
   let cps = [], cdone = false
+  let ips = [], idone = false
   for (i = 0; i < github.apiData.length; i++) {
     lps.push(github.getLanguageInfo(github.apiData[i].languages.url));
     cps.push(github.getContributorsInfo(github.apiData[i].contributors.url));
+    ips.push(github.getIssues(github.apiData[i].issues.url));
   }
+  /*
   Promise.all(lps)
     .then(function(ls) {
       for (i = 0; i < ls.length; i++) {
@@ -106,8 +159,19 @@ async function main(params) {
     .catch(function(e) {
       console.log(e)
     });
+  */
+ Promise.all(ips)
+  .then(function(is) {
+    for (i = 0; i < is.length; i++) {
+      github.apiData[i].issues.data = is[i];
+    }
+    finish()
+  })
+  .catch(function(e) {
+    console.log(e)
+  });
+
   function finish(){
-    console.log(JSON.stringify(github.apiData, null, 2));
     fs.writeFileSync('github-data.json', JSON.stringify(github.apiData, null, 2));
   }
 }
